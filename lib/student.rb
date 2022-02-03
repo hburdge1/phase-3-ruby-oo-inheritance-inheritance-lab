995ed1930d3034855327052638ad6cada36a1dc9
hburdge1 <95104319+hburdge1@users.noreply.github.com>
2022-02-03 08:54:53 -0800

    commit tree 47ccac7fbe72f871d5f758d1bd2adedbbd2cf66c

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require 'pry'
+class Student < User
+attr_accessor :knowledge
+    
+    def initialize
+        @knowledge=[]
+    end
+
+    def learn(str="Ruby framework Rails gem bundle update")
+        return @knowledge.push(str)
+    end
+
+    def knowledge
+       return @knowledge    
+    end
 
 end
